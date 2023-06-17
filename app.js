d5c50a268940f4c44c2885b7f113581c71834d7b
businessmajor <barrios.st@northeastern.edu>
2023-06-17 19:59:41 -0400

    somehow missing user controller. reimplemented per A5 homework

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ import cors from 'cors'
 import session from "express-session";
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./users/users-controller.js"
-import TuitsController from "./controllers/tuits/tuits-controller.js";
+import TuitsController from "./controllers/tuits-controller.js";
 import AuthController from "./users/auth-controller.js";
 
 const app = express()
 app.use(
     cors({
         credentials: true,
-        origin: "http://localhost:3000",
+        origin: "http://localhost:4000",
     })
 );
 app.use(
